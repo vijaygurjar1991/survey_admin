5dbbb630a764255a58ad20dee98f555627189e95
RishabhNMT <rishabh@nmttechnologies.com>
2023-10-05 23:44:27 +0530

    [Dashboard]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
