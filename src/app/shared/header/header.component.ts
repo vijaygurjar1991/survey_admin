64826cc40822e400cfec6cf6f7a0f6b62bb603ea
vijaydeep <vijaydeep@targeticon.com>
2023-12-18 13:35:30 +0530

    Profile Pic

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
 import { DataService } from 'src/app/service/data.service';
+import { UtilsService } from 'src/app/service/utils.service';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,23 @@ import { DataService } from 'src/app/service/data.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  public constructor(public themeService: DataService, private auth: AuthService,) {
+  public constructor(public themeService: DataService, private auth: AuthService,private util: UtilsService) {
 
   }
-
+  ngOnInit(): void {
+    this.getMyAccount()
+  }
   logOut() {
     this.auth.logout();
+  }
+  userId: any;
+  image: any;
+  getMyAccount() {
+    //this.userId = localStorage.getItem("userId");
+    this.userId = this.util.getUserId();
+    this.themeService.GetMyAccount(this.userId).subscribe((data: any) => {
+      console.log("inside header data", data) 
+      this.image = data.image
+    });
   }
 }
